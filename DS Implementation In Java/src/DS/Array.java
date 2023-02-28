029275ae944e328b43e9fa17f62d23634de5230f
Mohamed Ameen <mohamedameen.21.2001@outlook.com>
2023-02-28 23:44:11 +0530

    array completed

---
+++ DS Implementation In Java/src/DS/Array.java
@@ -0,0 +1,57 @@
+package DS;
+
+import java.util.Arrays;
+
+public class Array {
+    private int index;
+    private int[] arr;
+    private int arraySize;
+
+    public Array() {
+        arraySize = 10;
+        arr = new int[arraySize];
+    }
+
+    public Array(int length) {
+        arraySize = length;
+        arr = new int[arraySize];
+    }
+
+    public boolean add(int num) {
+        if (index >= arraySize) {
+            arraySize *= 2;
+            arr = Arrays.copyOf(arr, arraySize);
+        }
+        arr[index++] = num;
+        return true;
+    }
+
+    public int removeAt(int index) {
+        if (index >= this.index)
+            throw new IndexOutOfBoundsException();
+
+        int ele = arr[index];
+        for (int i = index; i < this.index - 1; i++)
+            arr[i] = arr[i + 1];
+
+        this.index--;
+        return ele;
+    }
+
+    public int indexOf(int num) {
+        for (int i = 0; i < index; i++)
+            if (arr[i] == num)
+                return i;
+        return -1;
+    }
+
+    public int length() {
+        return index;
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(Arrays.copyOf(arr, index));
+    }
+
+}
--- DS Implementation In Java/src/Main.java
+++ DS Implementation In Java/src/Main.java
@@ -1,5 +1,18 @@
+import DS.Array;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Array array = new Array(5);
+        array.add(10);
+        array.add(20);
+        array.add(30);
+        array.add(40);
+        array.add(50);
+        System.out.println(array.add(60));
+        System.out.println(array);
+        System.out.println(array.length());
+        System.out.println(array.indexOf(600));
+        System.out.println(array.removeAt(0));
+        System.out.println(array);
     }
 }
